1266c535ff0d43c12cefda91909784fc8204a428
Tben <benoti.san@gmail.com>
2023-07-29 23:18:34 +0100

    First Commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# Less watch for .less files
+
+---
+
+This package watches .less files and actively compiles them into .css files
+
+---
+
+## How to use
+
+There are few different ways to run your less compiler
+
+#### Basic usage
+
+```bash
+less-watcher --src ./folder --dst ./dist/less.css
+```
+
+This traverses the folder and searches for a `main.less` file. This file serves as the source for your bundled `.css` file. If you want to target a specific file, use:
+
+```bash
+less-watcher --src ./folder/src.less --dst ./dist/less.css
+```
+
+Or you can just watch an entire folder. In this case less-watch will searc for an entry file named `main.less`. Without this entry file, your compiler wouldn't work.
+
+```bash
+less-watcher --src ./folder --dst ./dist/less.css
+```
+
+#### Adding more source folders
+
+You can add more source folders by using a comma(`,`) separator. Example:
+
+```bash
+less-watcher --src ./folder-1,./folder-2 --dst ./dist/folder-1.css,./dist/folder-2.css
+```
+
+**_NOTE:_** Your `--dst` input must match the number of comma-separated folders in your `src` input.
+
+#### Advanced Features
+
+less-watcher has more advanced features to better fine-tune your files
+
+##### Ignore files
+
+Ignore files/folders by enlosing the names in braces. Eg `(general).less`
+
+##### Compile specific files
+
+If you're watching an entire folder, you can compile specific files in that folder to a stanalone file. Example if you create a file named `[test].less` in your watch directory, in your distribution directory there will be an extra file named `test.css`.
---
+++ dist/index.js
@@ -0,0 +1,46 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = __importDefault(require("fs"));
+const child_process_1 = require("child_process");
+const sourceFile = process.argv.indexOf("--src") >= 0 ? process.argv[process.argv.indexOf("--src") + 1] : null;
+const destinationFile = process.argv.indexOf("--dst") >= 0 ? process.argv[process.argv.indexOf("--dst") + 1] : null;
+console.log("Running Less compiler ...");
+if (!sourceFile || !destinationFile) {
+    console.log("ERROR => Missing source or destination file");
+    process.exit(1);
+}
+const sourceFiles = sourceFile.split(",");
+const dstFiles = destinationFile.split(",");
+for (let i = 0; i < sourceFiles.length; i++) {
+    const srcFolder = sourceFiles[i];
+    const dstFile = dstFiles[i];
+    fs_1.default.watch(srcFolder, { recursive: true }, (evtType, fileName) => {
+        if ((fileName === null || fileName === void 0 ? void 0 : fileName.match(/\(/)) || (fileName === null || fileName === void 0 ? void 0 : fileName.match(/\.js$/i))) {
+            return;
+        }
+        let finalSrcPath = `${srcFolder}/main.less`;
+        let finalDstPath = dstFile;
+        if (fileName === null || fileName === void 0 ? void 0 : fileName.match(/\[/)) {
+            const paths = fileName.split("/");
+            const targetPathFull = paths[paths.length - 1];
+            const targetPath = targetPathFull.replace(/\[|\]/g, "").replace(/\.less/, "");
+            const destinationFileParentFolder = dstFile.replace(/\/[^\/]+\.css$/, "");
+            const targetDstFilePath = `${destinationFileParentFolder}/${targetPath}.css`;
+            finalSrcPath = `${srcFolder}/${targetPathFull}`;
+            finalDstPath = targetDstFilePath;
+        }
+        (0, child_process_1.exec)(`lessc ${finalSrcPath} ${(finalDstPath === null || finalDstPath === void 0 ? void 0 : finalDstPath.match(/\.css$/)) ? finalDstPath : finalDstPath.replace(/\/$/, "") + "/_main.css"}`, (error, stdout, stderr) => {
+            if (error) {
+                console.log("ERROR =>", error.message);
+                if (!(evtType === null || evtType === void 0 ? void 0 : evtType.match(/change/i)) && fileName && fileName.match(/\[/)) {
+                    fs_1.default.unlinkSync(finalDstPath);
+                }
+                return;
+            }
+            console.log("Less Compilation \x1b[32msuccessful\x1b[0m!");
+        });
+    });
+}
